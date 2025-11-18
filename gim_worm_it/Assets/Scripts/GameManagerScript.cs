c00cee4cd7d189080be0358f883f7056a3ae4704
Theresia <theresiamonica2007@gmail.com>
2025-11-18 15:35:17 +0700

    Hewan bergerak ke stake
    semua hewan ke stake

--- gim_worm_it/Assets/Scripts/GameManagerScript.cs
+++ gim_worm_it/Assets/Scripts/GameManagerScript.cs
@@ -10,29 +10,61 @@ public class GameManagerScript : MonoBehaviour
     public GameObject kumbangPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
     public void ButtonPressed()
     {
-        if(Random.value <= percentageToSpawnWorm)
-        {
-            SpawnWorm();
-        }
+        if (Random.value <= percentageToSpawnWorm)
+            SpawnCreature(wormPrefab);
 
+        if (Random.value <= 0.1f) // peluang spawn semut
+            SpawnCreature(antPrefab);
+
+        if (Random.value <= 0.05f) // peluang spawn kumbang
+            SpawnCreature(kumbangPrefab);
     }
 
-    void SpawnWorm()
+    // Spawn semua jenis binatang 
+    void SpawnCreature(GameObject prefab)
     {
-        Debug.Log("Worm spawned");
-        Instantiate(wormPrefab, new Vector3(0,0,0), Quaternion.identity);
+        Vector3 spawnPos = GetSpawnFromScreenEdge();
+        GameObject creature = Instantiate(prefab, spawnPos, Quaternion.identity);
+
+        WormMovement wm = creature.GetComponent<WormMovement>();
+        if (wm != null)
+        {
+            wm.target = WoodenStake.transform;
+            wm.startSpeed = 1f;
+            wm.maxSpeed = 5f;
+            wm.acceleration = 0.5f;
+        }
+    }
+
+    // Spawn dari ujung layar: kiri, kanan, atau atas
+    Vector3 GetSpawnFromScreenEdge()
+    {
+        Vector3 spawnPos = Vector3.zero;
+        float zDistance = 0f; 
+
+        int edge = Random.Range(0, 3); 
+
+        switch (edge)
+        {
+            case 0: // kiri
+                spawnPos = Camera.main.ViewportToWorldPoint(new Vector3(0, Random.Range(0.3f, 1f), zDistance));
+                break;
+            case 1: // kanan
+                spawnPos = Camera.main.ViewportToWorldPoint(new Vector3(1, Random.Range(0.3f, 1f), zDistance));
+                break;
+            case 2: // atas
+                spawnPos = Camera.main.ViewportToWorldPoint(new Vector3(Random.Range(0f, 1f), 1, zDistance));
+                break;
+        }
+
+        spawnPos.z = 0; 
+        return spawnPos;
     }
 }
